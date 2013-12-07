9435887e058721949619b1b30f6c57c3dc110ad2
Paul T. Rekowski <Wixxel2@gmail.com>
2013-12-07 17:05:31 -0500

    Make a config change to the assets pipeline

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,8 @@ module SampleApp
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     #config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+
+    # Added to fix a heroku issue with the assets pipeline
+    config.assets.initialize_on_precompile = false
   end
 end
